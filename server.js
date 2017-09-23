79470cd7409e88ce1e4458fc8e3a010410b5129f
alexej.ferre@gmail.com <alexej.ferre@gmail.com>
2017-09-23 21:53:16 +0300

    MVC structure

--- server.js
+++ server.js
@@ -1,27 +1,17 @@
 // libs
 const express = require('express');
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-const ObjectID = require('mongodb').ObjectID;
+
+
+// myLibs
+var db = require('./db');
+var artistsController = require('./controllers/artists');
 
 // create sever
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// temp DB
-let db;
-var artists = [
-	{
-		id: 1,
-		name: 'Metallica'
-	},
-	{
-		id: 2,
-		name: 'Bon Jovi'
-	}
-];
 
 
 // Routes
@@ -31,64 +21,25 @@ app.get('/', function(req, res) {
 })
 
 
-app.get('/artists', function(req, res) {
-	db.collection('artists').find().toArray(function (err, docs) {
-		if (err) {
-			console.log(err);
-			return res.sendStatus(500);
-		}
-		res.send(docs);
-	})
-})
+app.get('/artists', artistsController.all);
 
-app.get('/artists/:id', function(req, res) {
-	db.collection('artists').findOne({ _id: ObjectID(req.params.id) }, function(err, doc) {
-		if (err) {
-			console.log(err);
-			return res.sendStatus(500);
-		}
-		res.send(doc);
-	})
-})
+app.get('/artists/:id', artistsController.findById);
 
 /* POST */
-app.post('/artists', function(req, res) {
-	var artist = {
-		name: req.body.name
-	};
-	db.collection('artists').insert(artist, function(err, result) {
-		if (err) {
-			console.log(err);
-			 return res.sendStatus(500);
-		}
-		res.send(artist);
-	})
-})
+app.post('/artists', artistsController.create);
 
 /* PUT */
-app.put('/artists/:id', function(req, res) {
-	var artist = artists.find(function(artist) {
-		return artist.id === parseInt(req.params.id);
-	});
-	artist.name = req.body.name;
-	res.sendStatus(200);
-})
+app.put('/artists/:id', artistsController.update);
 
 /* DELETE */
-app.delete('/artists/:id', function(req, res) {
-	artists.filter(function(artist) {
-		return artist.id !== parseInt(req.params.id);
-	})
-	res.sendStatus(200);
-})
+app.delete('/artists/:id', artistsController.delete);
 
 
 // Runserver
-MongoClient.connect('mongodb://localhost:27017/myapi', function (err, database) {
+db.connect('mongodb://localhost:27017/myapi', function (err) {
 	if (err) {
 		return console.log(err);
 	}
-	db = database;
 	app.listen(8081, function() {
 		console.log('Server is running');
 	})
